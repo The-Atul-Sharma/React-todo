3629fb1abf76bd670cc4d7ba080b54204f0c69aa
Atul Sharma <theatsharma@gmail.com>
2017-09-08 15:09:48 +0530

    Add files via upload

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG ={
+    apiKey: "AIzaSyAFtnCMwVcG-ki9IihApDBPJzxMraxa1dg",
+    authDomain: "todo-ccaca.firebaseapp.com",
+    databaseURL: "https://todo-ccaca.firebaseio.com",
+    projectId: "todo-ccaca",
+    storageBucket: "todo-ccaca.appspot.com",
+    messagingSenderId: "401266856338"
+}
---
+++ src/NoteForm/NoteForm.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './NoteForm.css';
+
+class NoteForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            newNoteContent: '',
+        };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.writeNote = this.writeNote.bind(this);
+    }
+
+    // When the user input changes, set the newNoteContent
+    // to the value of what's in the input box.
+    handleUserInput(e){
+        this.setState({
+            newNoteContent: e.target.value, // the value of the text input
+        })
+    }
+
+    writeNote(){
+        // call a method that sets the noteContent for a note to
+        // the value of the input
+        this.props.addNote(this.state.newNoteContent);
+
+        // Set newNoteContent back to an empty string.
+        this.setState({
+            newNoteContent: '',
+        })
+    }
+
+    render(){
+        return(
+            <div className="form">
+                <input type="text" placeholder="Add a task..."
+                value={this.state.newNoteContent} 
+                onChange={this.handleUserInput} required />
+                <button type="submit" onClick={this.writeNote}>+</button>
+            </div>
+        )
+    }
+}
+
+export default NoteForm;
